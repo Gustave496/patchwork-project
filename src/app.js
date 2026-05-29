6ea9e57810d3e823e0136dece9dd2d0b6117da6b
Gustave496 <sirop_sport_fraise@yahoo.fr>
2026-05-29 23:07:26 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ const modules = {
   `,
   'Deezer': () => `
     <h2>Deezer</h2>
-    <p>Voici votre intégration Deezer.</p>
     <iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/album/6158273" width="100%" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>
+    <p>Voici votre intégration Deezer.</p>
   `,
   'Ya combien combien': () => `
     <h2>Ya combien combien</h2>
